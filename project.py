b9d23d6b56aa502b71f0d01e02bf1cb255dcc1ea
Leslye Lu <Leslye@Leslyes-MacBook-Pro.local>
2016-03-23 06:44:57 -0700

    Final Project

--- project.py
+++ project.py
@@ -3,18 +3,19 @@ print "Hello! Welcome to Cheapest Flights around the World!"
 import urllib2
 import json
 
+print "Please enter the airport code for origin or destination, i.e. LAX"
 origin = raw_input ("Origin of destination ").upper()
 destination = raw_input ("Final destination ").upper()
 departure_day = raw_input ("Departure day (yyyy-mm-day) ")
 adult_passengers = raw_input ("How many adults are traveling? ")
+nosolutions = raw_input ("How options do you want? ")
 
-
-url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyCA9UXD4NP9oAOkA4I0WyHmoC-Q2HFCXyU"
+url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=XXXX"
 code = {
   "request": {
     "passengers": {
       "kind": "qpxexpress#passengerCounts",
-      "adultCount": adult_passengers,
+      "adultCount": int(adult_passengers),
     },
     "slice": [
       {
@@ -24,52 +25,31 @@ code = {
         "date": departure_day,
       }
     ],
-    # "refundable": false,
-    "solutions": 20
+    "solutions": int(nosolutions)
   }
 }
 jsonreq = json.dumps(code, encoding = 'utf-8')
+
 req = urllib2.Request(url, jsonreq, {'Content-Type': 'application/json'})
-# print req
-# print dir(req) 
+
 flight = urllib2.urlopen(req)
+
 response = flight.read()
 flight.close()
-# print(response)
 
 FareFlights = json.loads(response) #turns json into a dictionary
 
 
-FlightFares = FareFlights['trips']["tripOption"][0]["saleTotal"]
-# Departure = FareFlights['trips']["tripOption"][0]["saleTotal"]['slice'][2]['leg'][2]['departureTime']
-Departure_Time = FareFlights['trips']["tripOption"][0]['slice'][0]['segment'][0]['leg'][0]['departureTime']
-Arrival_Time = FareFlights['trips']["tripOption"][0]['slice'][0]['segment'][0]['leg'][0]['arrivalTime']
+Answerlist=[]
+for i in range(int(nosolutions)):
+  FlightFares = FareFlights['trips']["tripOption"][i]["saleTotal"]
+  Departure_Time = FareFlights['trips']["tripOption"][i]['slice'][0]['segment'][0]['leg'][0]['departureTime']
+  Arrival_Time = FareFlights['trips']["tripOption"][i]['slice'][0]['segment'][0]['leg'][0]['arrivalTime']
+  Carrier = FareFlights['trips']["tripOption"][i]['slice'][0]['segment'][0]['flight']['carrier']
+  Aircraft = FareFlights['trips']["tripOption"][i]['slice'][0]['segment'][0]['leg'][0]['aircraft']
 
-# # Arrival = FareFlights['trips']["tripOption"][0]["saleTotal"]
+  Answer = "Price: " + FlightFares + "  Departure Time: " + Departure_Time + "  Arrival Time: " + Arrival_Time + "  Airline: " + Carrier + "  Aircraft: " + Aircraft
+  Answerlist.append(Answer)
+  print Answerlist[i]
 
-# 
-# Carrier = FareFlights['trips']["data"][0]["carrier"][0]['name']
-# Aircraft = FareFlights['trips']["data"][0]["aircraft"][0]['name']
-
-
-print FlightFares
-print Departure_Time
-print Arrival_Time
-# print Carrier
-# print Aircraft
-
-# for keys, value 
-
-
-#  primary_colors = ('red', 'yellow', 'blue')
-
-# print primary_colors[-1]
-
-# print FlightFares = []
-# for saleTotal in response:
-#   for keys, value in film.items():
-#       if "fun_fact" in keys:
-#           movie_film_fact = film['title'] + ": " + value
-#           if movie_film_fact not in film_fact_list:
-#               film_fact_list.append(movie_film_fact)
 
